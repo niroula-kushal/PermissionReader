ce1c4f3654d6443113bb926ee20900d052f283d5
RehmatFalcon <niroulakushal31@gmail.com>
2020-04-19 17:10:42 +0545

    Feat: show tree guide

--- Program.cs
+++ Program.cs
@@ -9,23 +9,29 @@ namespace DynamicPermissionList
         {
             var json = File.ReadAllText(@"permissions.json");
             var permissions = PermissionJsonParser.parse(json);
+            Console.WriteLine(new String('*', 50));
             permissions.ForEach(renderPermission);
+            Console.WriteLine(new String('*', 50));
         }
 
         private static void renderPermission(Permission permission)
         {
-            Console.Write(new String('\t', permission.level - 1));
+            Console.Write(new String('-', permission.level == 1 ? 0 : 4));
             Console.Write(permission.name);
             if (permission.isLeaf)
             {
-                Console.Write("\t");
+                Console.Write("  ");
                 Console.Write($"[{permission.resolveName()}]");
             }
 
             Console.Write("\n");
             if (!permission.isLeaf)
             {
-                permission.children.ForEach(renderPermission);
+                permission.children.ForEach((child) => {
+                    Console.Write(new String(' ', (permission.level - 1) * 5));
+                    Console.Write("|");
+                    renderPermission(child);
+                });
             }
         }
     }
