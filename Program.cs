3daaa14a77784057243abdb82ee68feb92d4eabb
RehmatFalcon <niroulakushal31@gmail.com>
2020-04-19 10:38:14 +0545

    Feat: extract parser to class

--- Program.cs
+++ Program.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Text.Json;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 
 namespace DynamicPermissionList
 {
@@ -14,7 +8,7 @@ namespace DynamicPermissionList
         static void Main(string[] args)
         {
             var json = File.ReadAllText(@"permissions.json");
-            var permissions = parseJsonObject(json);
+            var permissions = PermissionJsonParser.parse(json);
             foreach (var permission in permissions)
             {
                 renderPermission(permission);
@@ -38,46 +32,6 @@ namespace DynamicPermissionList
                 {
                     renderPermission(child);
                 }
-            }
-        }
-
-        private static List<Permission> parseJsonObject(string json, Permission parent = null)
-        {
-            var jObj = JObject.Parse(json);
-            var topPermissions = new List<Permission>();
-            foreach (var item in jObj)
-            {
-                var permission = new Permission(item.Key);
-                if (parent != null)
-                {
-                    parent.addChild(permission);
-                }
-                else
-                {
-                    topPermissions.Add(permission);
-                }
-
-                switch (item.Value.Type)
-                {
-                    case JTokenType.Object:
-                        parseJsonObject(item.Value.ToString(), permission);
-                        break;
-                    case JTokenType.Array:
-                        parseJsonArray(item.Value.ToString(), permission);
-                        break;
-                }
-            }
-
-            return topPermissions;
-        }
-
-        private static void parseJsonArray(string json, Permission parent)
-        {
-            var jArr = JArray.Parse(json);
-            foreach (var item in jArr)
-            {
-                var permission = new Permission(item.Value<string>(), true);
-                parent.addChild(permission);
             }
         }
     }
